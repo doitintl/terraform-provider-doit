3f4db89324fb1eb66896bf0a79751d2a9e6755df
Olivier Boukili <boukili.olivier@gmail.com>
2025-09-09 15:06:10 +0200

    feat: added tests for reports

--- internal/doit/resource_report_test.go
+++ internal/doit/resource_report_test.go
@@ -59,7 +59,62 @@ func testAccReport(i int) string {
 resource "doit_report" "this" {
     name = "test-%d"
 	description = "test report"
+	config = {
+		metric = {
+		  type  = "basic"
+		  value = "cost"
+		}
+		include_promotional_credits = false
+		advanced_analysis = {
+		  trending_up   = false
+		  trending_down = false
+		  not_trending  = false
+		  forecast      = false
+		}
+		aggregation   = "total"
+		time_interval = "month"
+		dimensions = [
+		  {
+			id   = "year"
+			type = "datetime"
+		  },
+		  {
+			id   = "month"
+			type = "datetime"
+		  }
+		]
+		time_range = {
+		  mode            = "last"
+		  amount          = 12
+		  include_current = true
+		  unit            = "month"
+		}
+		filters = [
+		  {
+			inverse = false
+			id      = "attribution"
+			type    = "attribution"
+			values = [
+			  "1CE699ZdwN5CRBw0tInY"
+			]
+		  }
+		]
+		group = [
+		  {
+			id   = "BSQZmvX6hvuKGPDHX7R3"
+			type = "attribution_group"
+		  },
+		  {
+			id   = "cloud_provider"
+			type = "fixed"
+		  }
+		]
+		layout         = "table"
+		display_values = "actuals_only"
+		currency       = "USD"
+	  }
 }
+
 `, i)
 }
 
@@ -68,6 +123,60 @@ func testAccReportUpdate(i int) string {
 resource "doit_report" "this" {
     name = "test-%d"
 	description = "test report updated"
+	config = {
+		metric = {
+		  type  = "basic"
+		  value = "cost"
+		}
+		include_promotional_credits = false
+		advanced_analysis = {
+		  trending_up   = false
+		  trending_down = false
+		  not_trending  = false
+		  forecast      = false
+		}
+		aggregation   = "total"
+		time_interval = "month"
+		dimensions = [
+		  {
+			id   = "year"
+			type = "datetime"
+		  },
+		  {
+			id   = "month"
+			type = "datetime"
+		  }
+		]
+		time_range = {
+		  mode            = "last"
+		  amount          = 12
+		  include_current = true
+		  unit            = "month"
+		}
+		filters = [
+		  {
+			inverse = false
+			id      = "attribution"
+			type    = "attribution"
+			values = [
+			  "1CE699ZdwN5CRBw0tInY"
+			]
+		  }
+		]
+		group = [
+		  {
+			id   = "BSQZmvX6hvuKGPDHX7R3"
+			type = "attribution_group"
+		  },
+		  {
+			id   = "cloud_provider"
+			type = "fixed"
+		  }
+		]
+		layout         = "table"
+		display_values = "actuals_only"
+		currency       = "USD"
+	  }
 }
 `, i)
 }
