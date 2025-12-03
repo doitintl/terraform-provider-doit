dfca197ca48d82ec1489cdf081755264ab87ce66
Hannes Hayashi <hannes.h@doit.com>
2025-12-03 16:05:38 +0100

    docs: add comment explaining alternative timestamp generation for `start_period` in budget example.

--- examples/resources/doit_budget/resource.tf
+++ examples/resources/doit_budget/resource.tf
@@ -18,6 +18,8 @@ resource "doit_budget" "my_budget" {
   amount        = 100
   time_interval = "month"
   start_period  = time_static.now.unix * 1000 # This is a UNIX timestamp in milliseconds
+  # Instead of using a separate resource to get the unix timestamp, you can also use:
+  # provider::time::rfc3339_parse("2025-12-01T16:52:23.000Z") * 1000
   recipients = [
     "me@company.com"
   ]
