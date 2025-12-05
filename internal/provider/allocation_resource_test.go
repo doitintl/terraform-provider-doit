eb404e91be510c9429f06bef88ee9e38e9d65730
Hannes Hayashi <hannes.h@doit.com>
2025-12-05 08:55:47 +0100

    fix: Make `GroupAllocationRule` components optional and add `rule` or `rules` validation.

--- internal/provider/allocation_resource_test.go
+++ internal/provider/allocation_resource_test.go
@@ -2,6 +2,7 @@ package provider_test
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 
 	"math/rand/v2"
@@ -60,6 +61,61 @@ func TestAccAllocation(t *testing.T) {
 	})
 }
 
+func TestAccAllocation_Group(t *testing.T) {
+	n := rand.Int()
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProvidersProtoV6Factories,
+		PreCheck:                 testAccPreCheckFunc(t),
+		TerraformVersionChecks:   testAccTFVersionChecks,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAllocationGroup(n),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectNonEmptyPlan(),
+						plancheck.ExpectResourceAction(
+							"doit_allocation.group",
+							plancheck.ResourceActionCreate,
+						),
+					},
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						"doit_allocation.group",
+						tfjsonpath.New("allocation_type"),
+						knownvalue.StringExact("group")),
+				},
+			},
+			{
+				ResourceName:      "doit_allocation.group",
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateVerifyIgnore: []string{
+					"description",
+					"rules",
+				},
+			},
+		},
+	})
+}
+
+func TestAccAllocation_Validation(t *testing.T) {
+	n := rand.Int()
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProvidersProtoV6Factories,
+		PreCheck:                 testAccPreCheckFunc(t),
+		TerraformVersionChecks:   testAccTFVersionChecks,
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccAllocationValidation(n),
+				ExpectError: regexp.MustCompile("Invalid Attribute Combination"),
+			},
+		},
+	})
+}
+
 func testAccAllocationSingle(i int) string {
 	return fmt.Sprintf(`
 resource "doit_allocation" "this" {
@@ -108,6 +164,87 @@ resource "doit_allocation" "this" {
           }
        ]
     }
+}
+`, i)
+}
+
+func testAccAllocationGroup(i int) string {
+	return fmt.Sprintf(`
+resource "doit_allocation" "group" {
+    name = "test-group-%d"
+	description = "test allocation group"
+    rules = [
+        {
+            action = "create"
+       formula = "A AND B"
+       components = [
+        {
+           key    = "country"
+           mode   = "is"
+           type   = "fixed"
+           values = ["JP"]
+         },
+         {
+           key    = "project_id"
+           mode   = "is"
+           type   = "fixed"
+           values = ["test-k8s-project-468707"]
+          }
+       ]
+    },
+           {
+            action = "create"
+       formula = "A AND B"
+       components = [
+        {
+           key    = "country"
+           mode   = "is"
+           type   = "fixed"
+           values = ["US"]
+         },
+         {
+           key    = "project_id"
+           mode   = "is"
+           type   = "fixed"
+           values = ["test-k8s-project-468707"]
+          }
+       ]
+    }
+    ]
+}
+`, i)
+}
+
+func testAccAllocationValidation(i int) string {
+	return fmt.Sprintf(`
+resource "doit_allocation" "validation" {
+    name = "test-validation-%d"
+	description = "test allocation validation"
+    rule = {
+       formula = "A"
+       components = [
+        {
+           key    = "country"
+           mode   = "is"
+           type   = "fixed"
+           values = ["JP"]
+         }
+       ]
+    }
+    rules = [
+        {
+            action = "create"
+            formula = "A"
+            components = [
+                {
+                    key    = "country"
+                    mode   = "is"
+                    type   = "fixed"
+                    values = ["US"]
+                }
+            ]
+        }
+    ]
 }
 `, i)
 }
