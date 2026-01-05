a61044ff5f385a3a741fd7e2409970077e4af244
Hannes Hayashi <hannes.h@doit.com>
2026-01-05 11:07:37 +0100

    remove attribution again

--- internal/provider/model.go
+++ internal/provider/model.go
@@ -1,40 +1,5 @@
 package provider
 
-// Attribution - .
-type Attribution struct {
-	Id          string      `json:"id,omitempty"`
-	Name        string      `json:"name"`
-	Description string      `json:"description,omitempty"`
-	Formula     string      `json:"formula,omitempty"`
-	LastUpdated string      `json:"last_updated,omitempty"`
-	Components  []Component `json:"components,omitempty"`
-}
-
-// Component - .
-type Component struct {
-	TypeComponent string   `json:"type"`
-	Key           string   `json:"key"`
-	Values        []string `json:"values"`
-}
-
-// Attribution - .
-type AttributionGroup struct {
-	Id           string   `json:"id,omitempty"`
-	Name         string   `json:"name"`
-	Description  string   `json:"description,omitempty"`
-	LastUpdated  string   `json:"last_updated,omitempty"`
-	Attributions []string `json:"attributions"`
-}
-
-// Attribution - .
-type AttributionGroupGet struct {
-	Id           string        `json:"id,omitempty"`
-	Name         string        `json:"name"`
-	Description  string        `json:"description,omitempty"`
-	LastUpdated  string        `json:"last_updated"`
-	Attributions []Attribution `json:"attributions"`
-}
-
 type Budget struct {
 	// Alerts List of up to three thresholds defined as percentage of amount
 	Alerts []ExternalBudgetAlert `json:"alerts,omitempty"`
